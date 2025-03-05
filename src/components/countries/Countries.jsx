f43b00420a8e04fb648cb38af1967cb64c330314
sazid0101 <shahriarislamsazid@gmail.com>
2025-03-06 01:59:12 +0600

    Updating state in arrays and pass parameter

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -5,18 +5,34 @@ import "./countries.css"
 
 const Countries = () => {
     const [countries, setCountries] = useState([])
+    const [visitedCountries, setVisitedCountries] = useState([])
 
     useEffect(() =>{
         fetch("https://restcountries.com/v3.1/all")
         .then(Response => Response.json())
         .then(data => setCountries(data))
     },[])
+
+    const handleVisitedCountry = (country) =>{
+        // console.log("added")
+        // console.log(country)
+        const newVisitedCountries = [...visitedCountries, country]
+        setVisitedCountries(newVisitedCountries)
+    }
     return (
         <div >
             <h3>Countries: {countries.length}</h3>
+            <div>
+                <h4>Visited Country: {visitedCountries.length}</h4>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li>{country}</li>)
+                    }
+                </ul>
+            </div>
             <div className="grid-container">
             {
-                countries.map(country => <Country country = {country}></Country>)
+                countries.map(country => <Country country = {country} handleVisitedCountry = {handleVisitedCountry}></Country>)
             }
             </div>
         </div>
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './country.css'
-const Country = ({country}) => {
+const Country = ({country,  handleVisitedCountry}) => {
     // console.log(country)
     // console.log(country.name.common)
     const {name,flags,area,capital,timezones,population} = country
@@ -10,16 +10,20 @@ const Country = ({country}) => {
     const handleVisited = () =>{
         setVisited(!visited)
     }
+
     return (
-        <div className="box">
-            <h3>Name: {name.common}</h3>
+        <div className= {`box ${visited ? "visited-container" : "non-visited-container"}`}>
+            <h3 style={{color: visited ? "green" : "red"}}>Name: {name.common}</h3>
             <img src={flags.png} alt="" srcset="" />
             <h5>Capital: {capital}</h5>
             <h5>Area: {area}</h5>
             <h5>Timezones: {timezones[0]}</h5>
             <p>Population: {population}</p>
             <button onClick={handleVisited}>Visited</button>
+            {/* <button onClick={() => {handleVisited; handleVisitedCountry(country.name.common)}}>Visited</button> */}
             {visited ? "i have visited this country" : "i want to go"}
+            <br />
+            <button onClick={() => handleVisitedCountry(country.name.common)}>Mark Visited</button>
         </div>
     );
 };
